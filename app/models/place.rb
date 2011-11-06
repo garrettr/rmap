c8d4bbe8e3d78bbd120d7a031d6b94f72acaf599
Garrett Robinson <garrett.f.robinson@gmail.com>
2011-11-06 13:02:52 -0500

    Added annotate gem and annotated Place

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,2 +1,16 @@
+# == Schema Information
+#
+# Table name: places
+#
+#  id          :integer         not null, primary key
+#  name        :string(255)
+#  address     :string(255)
+#  description :text
+#  lat         :float
+#  lng         :float
+#  created_at  :datetime
+#  updated_at  :datetime
+#
+
 class Place < ActiveRecord::Base
 end
--- spec/models/place_spec.rb
+++ spec/models/place_spec.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: places
+#
+#  id          :integer         not null, primary key
+#  name        :string(255)
+#  address     :string(255)
+#  description :text
+#  lat         :float
+#  lng         :float
+#  created_at  :datetime
+#  updated_at  :datetime
+#
+
 require 'spec_helper'
 
 describe Place do
