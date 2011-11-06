9ba9e54c31ab1533641e858bacfd772b14c40078
Garrett Robinson <garrett.f.robinson@gmail.com>
2011-11-06 11:12:56 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rmap::Application.config.secret_token = '4db94f9ebad54a0ed73ead729610f3b751e3a461c27b1bc1085ee59be6cac54957852c2b5b57bfb2dc04f7b710ace83928ede2336d7dbf0ef315231f102c136d'
